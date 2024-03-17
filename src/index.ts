9eca746f04cc51284cfc590858e28c0883ff178f
ahxxm <i@ahxxm.com>
2024-03-17 05:10:57 +0000

    / to bind url once

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -23,10 +14,36 @@ export interface Env {
 	//
 	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
 	// MY_QUEUE: Queue;
+
+	TELEGRAM_BOT_TOKEN: String,
+}
+
+const webhookPath = "/telegram";
+
+async function bindTelegramWebhook(env: Env, url: URL): Promise<Response> {
+	const bindURL = `https://${url.host}${webhookPath}`;
+	const text = await fetch(
+		`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/setWebhook`,
+		{
+		  method: 'POST',
+		  headers: {
+			'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify({
+			url: bindURL,
+		  }),
+		},
+	).then((rsp) => rsp.text());
+	const rsp = `${text} \n the bind URL is: ${bindURL}`;
+	return new Response(rsp);
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const url = new URL(request.url);
+		if (url.pathname == "/") {
+			return await bindTelegramWebhook(env, url);
+		}
+		return new Response(url.pathname);
 	},
 };
